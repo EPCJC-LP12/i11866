cfa13898de831e86b697a018e99e5f88f5dc6643
Denis Petrov <i11866@epcjc.net>
2013-10-01 13:16:48 +0100

    ex3
    ainda por terminar

---
+++ ex3/ex3/Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        End
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim numero As Random = New Random()
+        
+    End Sub
+End Class
